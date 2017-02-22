6e1b40d56dec89288458994db9b35772fc57df53
Jerome Baek <jerome@fathomhealth.co>
2017-02-21 18:42:49 -0800

    FIX

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from setuptools import setup
 setup(
     name='pre_commit_dummy_package',
     version='0.0.0',
-    install_requires=['typed_ast', 'mypy']
+    install_requires=['mypy-lang', 'typed_ast']
 )
